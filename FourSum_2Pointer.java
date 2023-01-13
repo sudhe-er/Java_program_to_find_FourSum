9840e8de52fa1e0950a772b03de3cbd4c7a7fadc
sudhe-er <113851940+sudhe-er@users.noreply.github.com>
2023-01-13 20:22:15 +0530

    Update FourSum_2Pointer.java

--- FourSum_2Pointer.java
+++ FourSum_2Pointer.java
@@ -3,18 +3,18 @@ import java.util.*;
 public class FourSum_2Pointer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter n value: ");
-int n = sc.nextInt();
-System.out.println("Enter target value: ");
-int x = sc.nextInt();
-int[] arr = new int[n];
-System.out.println("Enter array Elements: ");
-	for(int i = 0; i < n; i++)
-		arr[i] = sc.nextInt();
-	findFourSum_2Pointer(arr, n, x);
-}
+		
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter n value: ");
+			int n = sc.nextInt();
+			System.out.println("Enter target value: ");
+			int x = sc.nextInt();
+			int[] arr = new int[n];
+			System.out.println("Enter array Elements: ");
+				for(int i = 0; i < n; i++)
+					arr[i] = sc.nextInt();
+				findFourSum_2Pointer(arr, n, x);
+				}
 public static void findFourSum_2Pointer(int[] a, int n, int x)
 {
 	int l, r;
